21e367b02dbec5a0e39ff4eb4ad86343c47a9dda
elizaveta <violetcactusonline@gmail.com>
2019-11-10 17:07:52 +0400

    unit tests

---
+++ src/main/java/UnitTests/SumTest.java
@@ -0,0 +1,17 @@
+package UnitTests;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.annotations.BeforeClass;
+
+public class SumTest {
+	private Calculator calculator;
+
+	@BeforeClass
+	public void setUp() {
+		calculator = new Calculator();
+	}
+
+	public void OnePlusTwoTest() {
+
+	}
+}
