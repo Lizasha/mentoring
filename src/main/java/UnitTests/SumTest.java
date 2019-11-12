a675976cd15375179d43da4b5d1c2d5d814ae9de
Liza <violetcactusonline@gmail.com>
2019-11-12 20:36:48 +0400

    i've tried to add data provider

--- src/main/java/UnitTests/SumTest.java
+++ src/main/java/UnitTests/SumTest.java
@@ -2,13 +2,24 @@ package UnitTests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.testng.annotations.DataProvider;
 
 public class SumTest extends BaseTest {
 
-	@Test
-	public void AddTwoValues() {
-		long result = calculator.sum(565,90000000);
-		boolean actual = (result == 90000565);
+	@Test() //data provider doesn't work, ask mentor
+	public void AddTwoValues(long firstValue, long secondValue, long expectedValue) {
+		long result = calculator.sum(firstValue,secondValue);
+		boolean actual = (result == expectedValue);
 		Assert.assertTrue("Result isn't correct!", actual);
+	}
+
+	@DataProvider(name = "valuesForSum")
+	public Object[][] valuesForSum() {
+		return new Object[][] {
+				{10,23,33},
+				{10,-1,9},
+				{0,0,0},
+				{-1,10,9}
+		};
 	}
 }
